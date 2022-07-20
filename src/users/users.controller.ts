174b250599359cbaa8512c2407549796b3bbdfaf
black996 <rharasis@yahoo.com>
2022-07-21 00:24:35 +0300

    inject usersService, add impl to createUser, error handling in case of duplicate email

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+    constructor(private usersService: UsersService) { }
+
     @Post()
-    createUser(@Body() body: CreateUserDto) {
-        console.log(body);
+    async createUser(@Body() body: CreateUserDto) {
+        try {
+            await this.usersService.create(body);
+        } catch (err) {
+            if (err instanceof Error && err.message.includes("UNIQUE constraint")) {
+                throw new BadRequestException("Email is already is use!");
+            }
+        }
+
     }
 }
