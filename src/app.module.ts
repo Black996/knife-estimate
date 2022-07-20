effcbbb7ed10aa0cd6124c3d3469ea9abc2ba59c
black996 <rharasis@yahoo.com>
2022-07-20 22:49:08 +0300

    connect entities at root

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EstimatesModule } from './estimates/estimates.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/users.entity';
+import { Estimate } from './estimates/estimates.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: "sqlite",
     database: "db.sqlite",
-    entities: [],
+    entities: [User, Estimate],
     synchronize: true
   }), UsersModule, EstimatesModule],
   controllers: [AppController],
