fbbff8de16d8865abe2988503bf40a5e6219f3c8
black996 <rharasis@yahoo.com>
2022-07-20 20:59:13 +0300

    establish db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EstimatesModule } from './estimates/estimates.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, EstimatesModule],
+  imports: [TypeOrmModule.forRoot({
+    type: "sqlite",
+    database: "db.sqlite",
+    entities: [],
+    synchronize: true
+  }), UsersModule, EstimatesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
