523f3e4c3791d5622ab243974918c4bf889eb7ea
black996 <rharasis@yahoo.com>
2022-07-20 23:16:20 +0300

    add createUser req body DTO, apply validation on req body

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    @Post()
+    createUser(@Body() body: CreateUserDto) {
+        console.log(body);
+    }
+}
