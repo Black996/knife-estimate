d0abdab47ee2624b5ebf4d5bbd66da09e49ad7eb
black996 <rharasis@yahoo.com>
2022-07-23 23:37:01 +0300

    impl crud for users resource in its service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { User } from './users.entity';
 
 @Injectable()
 export class UsersService {
     constructor(@InjectRepository(User) private repo: Repository<User>) { }
 
+    private async findUserByIdThenApplyCb<T>(id: number, cb?: (user?: User) => Promise<T>): Promise<T | User> {
+        const user = await this.repo.findOneBy({ id });
+        if (!user) {
+            throw new NotFoundException(`no such user`);
+        } else {
+            return cb ? cb(user): user;
+        }
+    }
+
     create(userData: { username: string; email: string; password: string }) {
         const user = this.repo.create(userData);
         return this.repo.save(user);
+    }
+
+    findOneById(id: number) {
+        return this.findUserByIdThenApplyCb(id);
+    }
+
+    async findOneByEmail(email: string) {
+        return this.repo.findOneBy({ email });
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        return this.findUserByIdThenApplyCb<UpdateResult>(id, (user) => {
+            Object.assign(user, attrs);
+            return this.repo.update(id,user);
+        })
+    }
+
+    remove(id: number) {
+        return this.findUserByIdThenApplyCb(id,(user)=>this.repo.remove(user));
     }
 }
