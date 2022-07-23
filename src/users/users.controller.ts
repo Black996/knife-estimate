54c96f8ab007bb71799c78f20e13443772f12246
black996 <rharasis@yahoo.com>
2022-07-23 23:34:05 +0300

    add CRUD for users resource

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,49 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser';
+import { UpdateUserDto } from './dtos/updateUser';
+import { User } from './users.entity';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
     constructor(private usersService: UsersService) { }
 
-    @Post()
+    @Post("signup")
     async createUser(@Body() body: CreateUserDto) {
         try {
-            await this.usersService.create(body);
+            return await this.usersService.create(body);
         } catch (err) {
             if (err instanceof Error && err.message.includes("UNIQUE constraint")) {
                 throw new BadRequestException("Email is already is use!");
             }
         }
-
     }
+
+    @Get(":id")
+    findUser(@Param('id') id:string){
+        return this.usersService.findOneById(parseInt(id));
+    }
+
+    @Get()
+    findUserByEmail(@Query('email') email:string){
+        return this.usersService.findOneByEmail(email);
+    }
+
+    @Patch(":id")
+    async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+        try {
+            return await this.usersService.update(parseInt(id), body);
+        } catch (err) {
+            if (err instanceof Error && err.message.includes("UNIQUE constraint")) {
+                throw new BadRequestException("Email is already is use!");
+            }
+
+        }
+    }
+
+    @Delete(":id")
+    deleteUser(@Param('id') id : string){
+        return this.usersService.remove(parseInt(id));
+    }
+
 }
