84cec33416ddd6a57c597b129bb09ea223bf7dda
black996 <rharasis@yahoo.com>
2022-09-09 17:55:03 +0300

    expose login endpoint and apply AuthGuard to it

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(AuthGuard('local'))
+  @Post('/auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
+
 }
