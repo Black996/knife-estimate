c54ddb0f1ee3e34b6f757a3f6f03250702edad1e
black996 <rharasis@yahoo.com>
2022-09-16 23:41:06 +0300

    configure session options, impl session based auth in the project

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { CommonHelpersModule } from 'src/common-helpers/common-helpers.module';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
+import { SessionSerializer } from './session.serializer';
 
 @Module({
-    imports:[UsersModule, PassportModule,CommonHelpersModule],
-    providers:[AuthService, LocalStrategy]
+    imports:[UsersModule, PassportModule,CommonHelpersModule,PassportModule.register({session:true})],
+    providers:[AuthService, LocalStrategy,SessionSerializer]
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as expressSession from 'express-session';
+import * as passport from 'passport';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  app.use(expressSession({
+    secret:configService.get('SESSION_SECRET'),
+    resave:false,
+    saveUninitialized:false
+  }))
+
+  app.use(passport.initialize())
+  app.use(passport.session())
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   await app.listen(3000);
 }
